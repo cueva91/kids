9abf4b49476672a25efa9557aeb24398689d34cf
cueva91 <fondocapitum2024@gmail.com>
2024-09-04 17:10:03 -0500

    add vercel file

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
+  dialectModule: require('mysql2')
 });
 
 sequelize
