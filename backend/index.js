947ca40e0d7ae2c08f27b06b5a487f3017d1bf58
cueva91 <fondocapitum2024@gmail.com>
2024-09-04 17:22:37 -0500

    edit import

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const sequelize = require('./config/database');
-const videoRoutes = require('./routes/videoRoutes');
+const videoRoutes = require('./routes/VideoRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
