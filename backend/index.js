83a48ee7dcfdb6f699daf993b91e81d608f023b6
cueva91 <fondocapitum2024@gmail.com>
2024-09-23 11:49:06 -0500

    nuevo

--- backend/index.js
+++ backend/index.js
@@ -3,32 +3,34 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const sequelize = require('./config/database');
 const videoRoutes = require('./routes/VideoRoutes');
-const pdfRoutes = require('./routes/PdfRoutes');  // Nueva ruta
-const path = require('path');  // Para manejar rutas de archivos
+const pdfRoutes = require('./routes/PdfRoutes');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Configuración de CORS
 app.use(cors({
-  origin: 'https://abckidslearning.com',  
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-  credentials: true
+  origin: 'https://abckidslearning.com',  // Permitir solicitudes desde tu frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
+  credentials: true,  // Permitir envío de cookies si es necesario
 }));
+
 app.use(bodyParser.json());
 
-// Middleware para servir archivos estáticos (para acceder a los PDFs desde la web)
+// Middleware para servir archivos estáticos
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Rutas
 app.use('/api/videos', videoRoutes);
-app.use('/api/pdf', pdfRoutes);  // Nueva ruta para PDFs
+app.use('/api/pdf', pdfRoutes);
 
 // Sincronizar la base de datos y arrancar el servidor
 sequelize.sync({ force: false })
   .then(() => {
     console.log('Database synchronized');
     app.listen(PORT, () => {
-      console.log(`Servidor corrieno en el puerto ${PORT}`);
+      console.log(`Servidor corriendo en el puerto ${PORT}`);
     });
   })
   .catch(err => console.log('Error al sincronizar base de datos:', err));
